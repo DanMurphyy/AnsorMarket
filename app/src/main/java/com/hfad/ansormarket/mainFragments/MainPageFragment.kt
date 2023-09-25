2d657dc6b41296a0b8d37f41ceb0c57b3b5acca4
DanMurphyy <highquality1996@gmail.com>
2023-09-26 01:24:55 +0500

    categories are working

--- app/src/main/java/com/hfad/ansormarket/mainFragments/MainPageFragment.kt
+++ app/src/main/java/com/hfad/ansormarket/mainFragments/MainPageFragment.kt
@@ -3,6 +3,7 @@ package com.hfad.ansormarket.mainFragments
 import android.app.ActionBar
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
@@ -10,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.bumptech.glide.Glide
+import com.google.android.material.chip.Chip
 import com.hfad.ansormarket.R
 import com.hfad.ansormarket.SharedViewModel
 import com.hfad.ansormarket.adapters.ItemListAdapter
@@ -30,6 +32,9 @@ class MainPageFragment : Fragment(), SearchView.OnQueryTextListener {
     private var mDialog: Dialog? = null
     private val adapter: ItemListAdapter by lazy { ItemListAdapter() }
 
+    private var currentSearchQuery: String? = null
+    private var currentCategory: String? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +44,7 @@ class MainPageFragment : Fragment(), SearchView.OnQueryTextListener {
         @Suppress("DEPRECATION", "DEPRECATION", "DEPRECATION")
         setHasOptionsMenu(true)
         mFirebaseViewModel.loadUserData(requireContext())
+        mFirebaseViewModel.fetchAllItems()
         showRecyclerView()
 
         return (binding.root)
@@ -47,13 +53,90 @@ class MainPageFragment : Fragment(), SearchView.OnQueryTextListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         liveUpdates()
+        filterAndCategory()
+    }
+
+    private fun filterAndCategory() {
+        val chipGroup = binding.chipGroup
+        val chipIds = listOf(
+            R.id.showAll,
+            R.id.Ichimliklar,
+            R.id.qandolat_mahsulotlari,
+            R.id.Sut_Mahsulotlari,
+            R.id.Tortlar_va_Pishiriqlar,
+            R.id.Konservalar,
+            R.id.Snaklar,
+            R.id.Mevalar,
+            R.id.Bolalar_Ovqatlari,
+            R.id.Un_Mahsulotlari,
+            R.id.Souslar,
+            R.id.Gosht,
+            R.id.Gigiena_vositalari,
+            R.id.Oshxona,
+            R.id.Yoglar
+        )
+
+
+        // Set click listeners for each chip to handle the selection manually
+        // Set click listeners for each chip to handle the selection manually
+        for (chipId in chipIds) {
+            val chip = chipGroup.findViewById<Chip>(chipId)
+            chip.setOnClickListener { view ->
+                // Iterate through the chips and uncheck all except the clicked one
+                for (id in chipIds) {
+                    val currentChip = chipGroup.findViewById<Chip>(id)
+                    Log.d("chips", "selectedChip: $currentChip")
+                    currentChip.isChecked = currentChip == view
+                }
+
+                if (chipId == R.id.showAll) {
+                    currentCategory = null
+                    allItem()
+                } else {
+                    val categoryText = (view as Chip).text.toString()
+                    currentCategory = categoryText
+                    category(categoryText)
+                    applyCategoryFilter(categoryText)
+                }
+            }
+        }
+    }
+
+    private fun allItem() {
+        mFirebaseViewModel.itemList.observe(viewLifecycleOwner) { itemList ->
+            adapter.setItems(itemList)
+        }
+    }
+
+    private fun category(categoryText: String) {
+        val categoryText = categoryText.trim()
+        val filteredItems = mFirebaseViewModel.itemList.value?.filter { item ->
+            val itemCategory = item.category.trim()
+            val isCategoryMatch = itemCategory.equals(categoryText, ignoreCase = true)
+            // Log the item's name, category, and match status
+            Log.d(
+                "chips",
+                "Item Name: ${item.nameItem}, Category: $itemCategory, Match: $isCategoryMatch"
+            )
+            isCategoryMatch
+        }
+
+        filteredItems?.let {
+            adapter.setItems(it)
+            Log.d("chips", "Selected category items: $it")
+        }
     }
 
     private fun liveUpdates() {
         mFirebaseViewModel.itemList.observe(viewLifecycleOwner) { itemList ->
-            adapter.setItems(itemList)
+            filteredItems = itemList.filter { item ->
+                currentCategory?.let {
+                    val itemCategory = item.category.trim()
+                    itemCategory.equals(it, ignoreCase = true)
+                } ?: true
+            }
+            adapter.setItems(filteredItems)
         }
-        mFirebaseViewModel.fetchAllItems()
         mFirebaseViewModel.myCartsLiveData.observe(viewLifecycleOwner) { myCartList ->
             adapter.setMyCartData(myCartList)
             mSharedViewModel.cartItemCount(requireView(), myCartList)
@@ -169,7 +252,29 @@ class MainPageFragment : Fragment(), SearchView.OnQueryTextListener {
     }
 
     private fun hideInfoDialog() {
+        liveUpdates()
         mDialog!!.dismiss()
+    }
+
+    private var filteredItems: List<Item> = emptyList()
+
+
+    private fun applyCategoryFilter(categoryText: String) {
+        val filteredItems = mFirebaseViewModel.itemList.value?.filter { item ->
+            val itemCategory = item.category.trim()
+            val isCategoryMatch = itemCategory.equals(categoryText, ignoreCase = true)
+            Log.d(
+                "chips",
+                "Item Name: ${item.nameItem}, Category: $itemCategory, Match: $isCategoryMatch"
+            )
+            isCategoryMatch
+        }
+
+        filteredItems?.let {
+            this.filteredItems = it // Update the filteredItems property
+            adapter.setItems(it)
+            Log.d("chips", "Selected category items: $it")
+        }
     }
 
     private fun toCart(currentItem: Item, quantity: Int) {
@@ -191,6 +296,7 @@ class MainPageFragment : Fragment(), SearchView.OnQueryTextListener {
 //                Toast.makeText(
 //                    requireContext(), getString(R.string.board_created), Toast.LENGTH_SHORT
 //                ).show()
+                currentCategory?.let { applyCategoryFilter(it) }
             } else {
                 Toast.makeText(
                     requireContext(), getString(R.string.board_creating_error), Toast.LENGTH_SHORT
@@ -226,7 +332,7 @@ class MainPageFragment : Fragment(), SearchView.OnQueryTextListener {
     }
 
     override fun onQueryTextSubmit(query: String?): Boolean {
-        if (query != null) {
+        if (!query.isNullOrBlank()) {
             searchThroughDatabase(query)
         }
 
@@ -235,28 +341,25 @@ class MainPageFragment : Fragment(), SearchView.OnQueryTextListener {
 
 
     override fun onQueryTextChange(query: String?): Boolean {
-        if (query != null) {
+        if (!query.isNullOrBlank()) {
             searchThroughDatabase(query)
+        } else if (!currentSearchQuery.isNullOrBlank()) {
+            searchThroughDatabase(currentSearchQuery!!)
         }
 
         return true
     }
 
     private fun searchThroughDatabase(query: String) {
-        val searchQuery =
-            query.trim() // Remove leading and trailing whitespace for a more precise search
-
+        currentSearchQuery = query.trim()
         val filteredItems = mFirebaseViewModel.itemList.value?.filter { item ->
-            // You can customize the condition for filtering here
-            item.nameItem.contains(
-                searchQuery,
-                ignoreCase = true
-            ) // Case-insensitive search by name
+            item.nameItem.contains(currentSearchQuery ?: "", ignoreCase = true)
         }
 
         filteredItems?.let {
             adapter.setItems(it)
         }
     }
+
 
 }
